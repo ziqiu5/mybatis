b02610e2fe84812e4386da4523b4f93c6907b0e4
ziqiu <2775333199@qq.com>
2023-01-13 12:37:07 +0800

    master fifth-commit

--- module02_git_test/src/main/java/com/atguigu/git/GitTest.java
+++ module02_git_test/src/main/java/com/atguigu/git/GitTest.java
@@ -18,6 +18,7 @@ public class GitTest {
         System.out.println("hello,git 2 !");
         System.out.println("hello,git 3 !");
         System.out.println("hello,git 4 !");
+        System.out.println("master test !");
     }
 
 
