b1ac2ff45919205c5d5d4ec3fbee0705e940da94
ziqiu <2775333199@qq.com>
2023-01-13 12:21:34 +0800

    mybatis-fourth-commit

--- module02_git_test/src/main/java/com/atguigu/git/GitTest.java
+++ module02_git_test/src/main/java/com/atguigu/git/GitTest.java
@@ -16,6 +16,7 @@ public class GitTest {
     public static void main(String[] args){
         System.out.println("hello,git 1 !");
         System.out.println("hello,git 2 !");
+        System.out.println("hello,git 3 !");
     }
 
 
