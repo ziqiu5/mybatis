a843f5c98f945717f9eb0307ee42aff23056e5d6
ziqiu <2775333199@qq.com>
2023-01-13 11:49:10 +0800

    mybatis-second-commit

---
+++ module02_git_test/src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,21 @@
+package com.atguigu.git;
+
+/**
+ * ClassName: GitTest
+ * Package: com.atguigu.git
+ * Description:
+ *
+ * @author ziqiu
+ * @Create: 2023/1/13 - 11:48  11:48
+ * @Version: v1.0
+ */
+
+
+public class GitTest {
+
+    public static void main(String[] args){
+        System.out.println("hello,git 1 !");
+    }
+
+
+}
