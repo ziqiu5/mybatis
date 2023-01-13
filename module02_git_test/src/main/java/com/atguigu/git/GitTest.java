e23879aba05b9ffd50be7b1d0a303f8d52a6184d
ziqiu <2775333199@qq.com>
2023-01-13 12:31:51 +0800

    hot-fix first-commit

--- module02_git_test/src/main/java/com/atguigu/git/GitTest.java
+++ module02_git_test/src/main/java/com/atguigu/git/GitTest.java
@@ -17,6 +17,7 @@ public class GitTest {
         System.out.println("hello,git 1 !");
         System.out.println("hello,git 2 !");
         System.out.println("hello,git 3 !");
+        System.out.println("hello,git 4 !");
     }
 
 
